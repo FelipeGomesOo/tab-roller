1bfb20270d6933a599554fc1e06c5f5b2b052f5e
Felipe Gomes <contato@felipe-gomes.com>
2024-02-09 08:57:18 -0300

    Remove get user ffrom Login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,6 @@ import LoginForm from "@/app/ui/login/LoginForm";
 import { getUserByEmail } from '@/app/lib/data';
 
 export default async function  LoginPage() {
-  const theUser = await getUserByEmail('contato@felipe-gomes.com')
   return (
     <LoginForm />
 )};
