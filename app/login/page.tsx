4e184f7d1da46c9f4f1697500166bc925cd7d0ba
Felipe Gomes <contato@felipe-gomes.com>
2024-02-09 08:56:39 -0300

    Remove Get user test

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,5 +4,5 @@ import { getUserByEmail } from '@/app/lib/data';
 export default async function  LoginPage() {
   const theUser = await getUserByEmail('contato@felipe-gomes.com')
   return (
-    <LoginForm theUser={theUser}/>
+    <LoginForm />
 )};
--- app/ui/login/LoginForm.tsx
+++ app/ui/login/LoginForm.tsx
@@ -4,7 +4,7 @@ import { authenticate } from '@/app/lib/actions';
 import { useFormInput } from '@/app/lib/hooks';
 import { Button } from '@/app/ui/button'; 
 import {User} from '@/app/lib/definitions';
-export default function LoginForm({theUser} : {theUser: User}) {
+export default function LoginForm() {
  
     const { pending } = useFormStatus();
     const [errorMessage, dispatch] = useFormState(authenticate, undefined);
@@ -12,8 +12,7 @@ export default function LoginForm({theUser} : {theUser: User}) {
     const passwordProps = useFormInput({ initialValue: "" });
 
   return (
-    <form action={dispatch}>
-        <h1>{theUser.name}</h1>
+    <form action={dispatch}> 
         <label htmlFor="email">Username:</label>
         <input  
             id="email"
