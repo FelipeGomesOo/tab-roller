b369aa4fc8618a9f2df059b3d812e9302b2b2e9b
Felipe Gomes <contato@felipe-gomes.com>
2024-02-20 12:13:43 -0300

    feat: get data for create song form

--- app/(LoggedInArea)/song/create/page.tsx
+++ app/(LoggedInArea)/song/create/page.tsx
@@ -1,11 +1,19 @@
 import CreateSongForm from '@/components/createSong/CreateSongForm';
 import {getArtists} from '@/lib/data';
+import { auth } from "@/auth";
+import { getUserByEmail } from '@/lib/data';
 
 export default async function CreateSongPage() { 
   let artists = await getArtists();
-  
+  let session = await auth();
+  let userEmail = session?.user?.email;
+  let loggedUser = userEmail && await getUserByEmail(userEmail) 
+  let userID = loggedUser?.id;
+
   return (
-    <>
-     <CreateSongForm artists={artists} /> 
-    </>
+    <div className=' pt-20'>
+      <div className="container mx-auto"> 
+        <CreateSongForm artists={artists} userId={userID} /> 
+      </div>
+    </div>
 )};
